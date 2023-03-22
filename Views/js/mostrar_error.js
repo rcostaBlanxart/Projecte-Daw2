3e2f8b0047b33c9fce92ee1d48d4fc514a273ebb
rcostaBlanxart <120482428+rcostaBlanxart@users.noreply.github.com>
2023-03-22 16:56:26 +0100

    Delete Views directory

--- Views/js/mostrar_error.js
+++
@@ -1,7 +0,0 @@
-function mostrar_confirmacion(id) {
-    document.getElementById(id).style.display = "block";
-}
-
-function ocultar_confirmacion(id) {
-    document.getElementById(id).style.display = "none";
-}
